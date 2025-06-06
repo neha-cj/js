b049fe1da2d909c6a1cb300a08c0f44cca8b7a67
Neha C J <119199146+neha-cj@users.noreply.github.com>
2025-06-06 15:58:31 +0530

    Create script.js

---
+++ slider/script.js
@@ -0,0 +1,25 @@
+const prev= document.querySelector('.prev');
+const next=document.querySelector('.next');
+const wrap = document.querySelector('.img-wrap');
+const imgs = document.querySelectorAll('.img-wrap img');
+
+let idx=0;
+function showImg(){
+    if(idx>=imgs.length) idx=0;
+    if(idx<0) idx=imgs.length-1;
+    wrap.style.transform = `translateX(-${idx * 100}%)`;
+}
+next.addEventListener('click',()=>{
+    idx++;
+    showImg();
+});
+prev.addEventListener('click',()=>{
+    idx--;
+    showImg();
+});
+
+setInterval(()=>{
+    idx++;
+    showImg();
+},3000);
+showImg();
